5412a4acfc89f7fe36a33d6e4c2d35044ebd0504
Jakob Edéus <jakob@edeus.se>
2019-09-09 09:46:05 +0200

    fix email confirmation CB FW

--- api/postBooking.php
+++ api/postBooking.php
@@ -68,5 +68,13 @@ header("Content-Type:application/json");
             ]);
         }
     }
+
+
+    $msg = "First line of text\nSecond line of text";
+    // use wordwrap() if lines are longer than 70 characters
+    $msg = wordwrap($msg,70);
+    // send email
+    mail("jakob@edeus.se","My subject",$msg);
+    
     
 ?>
